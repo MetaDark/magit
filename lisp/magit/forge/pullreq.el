d13b0fe93951764ea96aafe8be1bae5663f09cff
Jonas Bernoulli <jonas@bernoul.li>
2018-06-15 12:37:00 -0500

    magit-{issue,pullreq}-visit: New command aliases

--- lisp/magit/forge/pullreq.el
+++ lisp/magit/forge/pullreq.el
@@ -122,6 +122,8 @@
   (interactive (list (magit-read-pullreq "Browse pull-request")))
   (browse-url (magit-forge--format-url pullreq 'pullreq-url-format)))
 
+(defalias 'magit-pullreq-visit 'magit-pullreq-browse)
+
 ;;; Utilities
 
 (defun magit-read-pullreq (prompt)
@@ -150,6 +152,7 @@
 (defvar magit-pullreq-section-map
   (let ((map (make-sparse-keymap)))
     (define-key map [remap magit-browse-thing] 'magit-pullreq-browse)
+    (define-key map [remap magit-visit-thing]  'magit-pullreq-visit)
     map))
 
 (defun magit-insert-pullreqs ()
