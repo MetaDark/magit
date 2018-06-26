055a51874d04e7f9c3a5fb2463f1f960847a0ef5
Jonas Bernoulli <jonas@bernoul.li>
2018-06-26 02:04:45 -0500

    [DUMP] magit-{issue,pullreq}-visit: Implement

--- lisp/magit/forge/issue.el
+++ lisp/magit/forge/issue.el
@@ -95,7 +95,11 @@
   (interactive (list (magit-read-issue "Browse issue")))
   (browse-url (magit-forge--format-url issue 'issue-url-format)))
 
-(defalias 'magit-issue-visit 'magit-issue-browse)
+;;;###autoload
+(defun magit-issue-visit (issue)
+  (interactive (list (magit-read-issue "View issue")))
+  (let ((magit-generate-buffer-name-function 'magit-forge-topic-buffer-name))
+    (magit-mode-setup-internal #'magit-forge-topic-mode (list issue) t)))
 
 ;;;###autoload
 (defun magit-issue-create ()
@@ -155,3 +159,110 @@
 ;;; _
 (provide 'magit/forge/issue)
 ;;; magit/forge/issue.el ends here
+;;; TODO markdown
+
+(require 'markdown-mode nil t)
+
+;;; TODO magit/forge/topic.el
+;;;; Topic Mode
+
+(define-derived-mode magit-forge-topic-mode magit-mode "Topic" "View a topic.")
+
+(defvar magit-post-section-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-e") 'magit-forge-edit-post)
+    map))
+
+(defun magit-forge-topic-refresh-buffer (topic)
+  (magit-set-header-line-format
+   (format "#%s: %s"
+           (oref topic number)
+           (oref topic title)))
+  (magit-insert-section (topicbuf)
+    (dolist (post (cons topic (oref topic posts)))
+      (with-slots (author created body) post
+        (magit-insert-section (post post)
+          (let ((heading
+                 (format
+                  "%s %s\n"
+                  (propertize author  'face 'bold)
+                  (propertize created 'face 'italic))))
+            (add-face-text-property 0 (length heading)
+                                    'magit-diff-hunk-heading t heading)
+            (magit-insert-heading heading))
+          (insert (magit-forge--fontify-markdown body) "\n\n"))))))
+
+(defun magit-forge-topic-buffer-name (_mode topic)
+  (with-slots (owner name)
+      (magit-forge-get-project topic)
+    (format "*%s/%s #%d*" owner name (oref topic number))))
+
+;;;; Utilities
+
+;; KLUDGE Generalize this.
+(defun magit-forge-highlight-post (section &optional selection)
+  (when (eq (oref section type) 'post)
+    (dolist (section (or selection (list section)))
+      (magit-section-make-overlay
+       (oref section start)
+       (or (oref section content)
+           (oref section end))
+       'magit-diff-hunk-heading-highlight))
+    nil))
+
+(defun magit-forge--fontify-markdown (text)
+  (with-temp-buffer
+    (delay-mode-hooks
+      (if (fboundp 'gfm-view-mode)
+          (gfm-view-mode)
+        (text-mode)))
+    (insert text)
+    (font-lock-ensure)
+    (fill-region (point-min) (point-max))
+    (buffer-string)))
+
+;;; TODO magit/forge/post.el
+;;;; Post Mode
+
+(defun magit-post-at-point ()
+  (magit-section-when (issue pullreq post)))
+
+(defvar magit-forge-post-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'magit-forge-post-submit)
+    (define-key map (kbd "C-c C-k") 'magit-forge-post-cancel)
+    map))
+
+(define-minor-mode magit-forge-post-mode
+  "Edit a post.")
+
+(defvar-local magit-forge-buffer-post nil)
+
+(defun magit-forge-edit-post (post)
+  (interactive (list (magit-post-at-point)))
+  (let ((file (magit-git-dir (convert-standard-filename
+                              (concat "magit/posts/" (oref post id))))))
+    (when (file-exists-p file)
+      (delete-file file))
+    (if-let (buf (find-buffer-visiting file))
+        (pop-to-buffer buf)
+      (make-directory (file-name-directory file) t)
+      (find-file-other-window file)
+      (erase-buffer) ; TODO offer to use draft
+      (save-excursion
+        (insert (oref post body)))
+      (if (fboundp 'gfm-mode)
+          (gfm-mode)
+        (text-mode))
+      (visual-line-mode))
+    (setq magit-forge-buffer-post post)
+    (magit-forge-post-mode)))
+
+(defun magit-forge-post-submit ()
+  (interactive)
+  (cl-typecase magit-forge-buffer-post
+    (magit-forge-issue-post (message "post"))))
+
+(defun magit-forge-post-cancel ()
+  (interactive)
+  (kill-buffer))
